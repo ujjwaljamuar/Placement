654e207d2a7f37200c24f00005f52a6eede1078b
Ujjwal Jamuar <ujjwalj12222@gmail.com>
2021-08-10 21:53:52 +0530

    *

---
+++ Sorting/SelectionSort.java
@@ -0,0 +1,34 @@
+public class SelectionSort {
+
+    public void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public void selectionsrt(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[min]) {
+                    min = j;
+                }
+            }
+            int temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        SelectionSort ss = new SelectionSort();
+
+        int[] arr = { 5, 1, 2, 9, 10 };
+        ss.printArray(arr);
+
+        ss.selectionsrt(arr);
+        ss.printArray(arr);
+    }
+}
