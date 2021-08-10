dfc2a9cddf8650728e02bfc5211b7a580bbac789
Ujjwal Jamuar <ujjwalj12222@gmail.com>
2021-08-10 19:46:49 +0530

    *

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -1,14 +1,16 @@
 public class BubbleSort {
 
-    /* compare adjacent elements and swap
-    */
+    /*
+     * compare adjacent elements and swap
+     */
 
-    public void print(int[] arr){
+    public void print(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
         System.out.println();
     }
+
     public void isSwapped(int[] arr) {
         int n = arr.length;
         boolean isSwapped;
@@ -16,10 +18,10 @@ public class BubbleSort {
         for (int i = 0; i < n; i++) {
             isSwapped = false;
             for (int j = 0; j < n - 1 - i; j++) {
-                if (arr[j] > arr[j+1]) {
+                if (arr[j] > arr[j + 1]) {
                     int temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
                     isSwapped = true;
                 }
             }
@@ -28,9 +30,10 @@ public class BubbleSort {
             }
         }
     }
+
     public static void main(String[] args) {
         BubbleSort bs = new BubbleSort();
-        int[] arr = {4,1,5,8,9,2,3};
+        int[] arr = { 4, 1, 5, 8, 9, 2, 3 };
 
         bs.print(arr);
 
