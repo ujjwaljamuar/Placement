982678ad3946bbd75afc266bcf89943ea859a685
Ujjwal Jamuar <ujjwalj12222@gmail.com>
2021-08-10 19:32:43 +0530

    *

---
+++ Sorting/BubbleSort.java
@@ -0,0 +1,38 @@
+public class BubbleSort {
+
+    public void print(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+    public void isSwapped(int[] arr) {
+        int n = arr.length;
+        boolean isSwapped;
+
+        for (int i = 0; i < n; i++) {
+            isSwapped = false;
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (arr[j] > arr[j+1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                    isSwapped = true;
+                }
+            }
+            if (isSwapped = false) {
+                break;
+            }
+        }
+    }
+    public static void main(String[] args) {
+        BubbleSort bs = new BubbleSort();
+        int[] arr = {4,1,5,8,9,2,3};
+
+        bs.print(arr);
+
+        System.out.print("After Bubble Sort : ");
+        bs.isSwapped(arr);
+        bs.print(arr);
+    }
+}
