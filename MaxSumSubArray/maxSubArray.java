5b7553b1a237608d14376e0ddd831ed1d2179396
Ujjwal Jamuar <ujjwalj12222@gmail.com>
2021-08-15 12:17:54 +0530

    *

---
+++ MaxSumSubArray/maxSubArray.java
@@ -0,0 +1,28 @@
+package MaxSumSubArray;
+
+public class maxSubArray {
+
+    public int MaxSumSArr(int[] arr) {
+        int maxSoFar = arr[0];
+        int currentMax = arr[0];
+        int n = arr.length;
+        for (int i = 1; i < n; i++) {
+            currentMax = currentMax + arr[i];
+            if (currentMax < arr[i]) {
+                currentMax = arr[i];
+            }
+            if (maxSoFar < currentMax) {
+                maxSoFar = currentMax;
+            }
+        }
+        return maxSoFar;
+    }
+
+    public static void main(String[] args) {
+        maxSubArray msa = new maxSubArray();
+        int[] arr = { 4, 3, -2, 6, -12, 7, -1, 6 };
+
+        System.out.println(msa.MaxSumSArr(arr));
+
+    }
+}
