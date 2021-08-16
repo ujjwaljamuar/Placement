29a7d54ae6431b8c6dbc66ccc924eac289b8f3e8
Ujjwal Jamuar <ujjwalj12222@gmail.com>
2021-08-16 21:51:53 +0530

    *

--- PracticeQuestions/RemoveVowels.java
+++ PracticeQuestions/RemoveVowels.java
@@ -1,18 +1,20 @@
 import java.util.Set;
 
 public class RemoveVowels {
-    public static String removeVow(String str){
-        Set<Character> vowels = Set.of('a','e','i','o','u');
+    public static String removeVow(String str) {
+        Set<Character> vowels = Set.of('a', 'e', 'i', 'o', 'u');
         StringBuilder sb = new StringBuilder();
 
         char[] charArr = str.toCharArray();
-        for (Character ch: charArr) {
+
+        for (char ch : charArr) {
             if (!vowels.contains(ch)) {
                 sb.append(ch);
             }
         }
         return sb.toString();
     }
+
     public static void main(String[] args) {
         System.out.println(removeVow("ujjwal jamuar"));
     }
